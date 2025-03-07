1e12bd9ec091f46d233cc01b52471f8a26fa5a25
Peter Gyan <petergyan001@gmail.com>
2025-03-06 21:56:15 -0400

    upd2

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 function getAverageGrade(student, course) {
   let sum = 0;
-  for (let course of student) {
-    sum += course;
+  for (let transRec of student.transcript) {
+      if (transRec.course === course) {
+        for (let grade of transRec.grades) {
+          sum += grade;
+        }
+        return sum / 3;
+      }
   }
+  return -1;
+}
+
+function getAssignmentMark(student, course, num) {
+  for (let transRec of student.transcript) {
+      if (transRec.course === course) {
+          if (num >= 1 && num <= 3) {
+              return transRec.grades[num - 1]; 
+          }
+      }
+  }
+  return -1;
+}
+
+function averageAssessment(students, courseName, assignment) {
+  let sum = 0;
+  let count = 0;
+
+  for (let student of students) {
+      let mark = getAssignmentMark(student, courseName, assignment);
+      if (mark !== -1) {
+          sum += mark;
+          count++;
+      }
+  }
+
+  if (count === 0) {
+      return -1;  
+  }
+
+  return sum / count;
 }
 
 //object literal
@@ -51,3 +87,6 @@ let paul = {
 
 
 const students = [bob, sally, paul];
+console.log(getAverageGrade(bob, 'INFO 1603'));
+console.log(getAssignmentMark(sally, 'INFO 1601', 2));
+console.log(averageAssessment(students, 'INFO 1601', 2));
